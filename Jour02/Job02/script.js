56a90cab370878b9f3aff84196ea575bea6c4021
Morad <morad.labrid@laplateforme.io>
2021-03-09 11:06:32 +0100

    Job02 show hide

---
+++ Jour02/Job02/script.js
@@ -0,0 +1,7 @@
+function showhide() {
+    var text = document.getElementById('citation');
+
+    if (text.style.display === "none") {
+        text.style.display = "block";
+    } else text.style.display = "none";
+}
