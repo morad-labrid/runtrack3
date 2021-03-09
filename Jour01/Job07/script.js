1fee7e59761c67c21d6d37ab1da6d3110368416b
Morad <morad.labrid@laplateforme.io>
2021-03-09 10:05:38 +0100

    Job 7 jour ferié

---
+++ Jour01/Job07/script.js
@@ -0,0 +1,20 @@
+function jourtravaille(date) {
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    var ferier = ["1-1", "5-4", "1-5", "8-5", "13-5", "24-5", "14-7", "15-8", "1-11", "25-12"];
+    const day = new Date(date);
+
+    const date1 = day.getDate();
+    const date2 = day.getMonth() + 1;
+    var date3 = date1 + '-' + date2;
+    var text = "";
+    var i;
+
+    if (ferier.includes(date3)) {
+        text += day.toLocaleDateString('fr-FR', options) + " est un jour ferié<br>";
+    }
+    if (day.getDay() == 0 || day.getDay() == 6) {
+        text += "Non " + day.toLocaleDateString('fr-FR', options) + " est un week-end<br>";
+    } else text += "oui " + day.toLocaleDateString('fr-FR', options) + " est un jour travaillé<br>";
+
+    document.getElementById('demo').innerHTML = text;
+}
