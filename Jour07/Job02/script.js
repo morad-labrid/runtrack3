6c0ce7daba1c40af8eeb829daabfc22432efec7f
Morad <morad.labrid@laplateforme.io>
2021-03-18 21:33:10 +0100

    edit jour 07

--- Jour07/Job02/script.js
+++ Jour07/Job02/script.js
@@ -2,3 +2,10 @@ $("#changeText").click(function() {
     var text = document.getElementById('text').textContent;
     console.log(text);
 })
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function() {
+    myInput.focus()
+})
